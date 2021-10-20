585e278bdd4c799046923bf6de1b4fff4ece7af5
ace4ence <92896743+ace4ence@users.noreply.github.com>
2021-10-21 08:50:06 +0900

    Add files via upload

---
+++ Asm.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <Windows.h>
+
+int main()
+{
+	SetConsoleTitleA("asm kernel");
+
+	const char* kernemessage = "asm kernel";
+	const char* k_msg = "this is a simple asm kernel";
+	const char* k_msg_cap = "ace4ence";
+
+	_asm
+	{
+		mov eax, kernemessage // system calling
+		push eax
+		call printf
+		pop eax
+		mov eax, 0
+		mov ebx, k_msg_cap
+		mov edx, k_msg
+		mov edx, 0 // return 값 edx
+		push eax
+		push ebx
+		push edx
+		call MessageBoxA
+		pop edx
+		pop ecx
+		pop ebx
+		pop eax // eax 0
+	}
+}
